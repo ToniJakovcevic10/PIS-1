3ed45a96e4bc48204bc9b32832fce59fbbaa819b
student <Student@B420-01.fesb.hr>
2021-03-22 18:16:35 +0100

    Add project files.

---
+++ tjakov00_PIS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tjakov00_PIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
